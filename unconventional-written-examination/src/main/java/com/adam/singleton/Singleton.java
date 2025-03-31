10999b2c661e95a236eda7c6035f9e3179b63064
leochame <leocham.cn@gmail.com>
2025-03-31 23:05:45 +0800

    快排序

---
+++ unconventional-written-examination/src/main/java/com/adam/singleton/Singleton.java
@@ -0,0 +1,30 @@
+package com.adam.singleton;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public final class Singleton implements Serializable {
+    private static volatile  Singleton instance = null;
+
+    private Singleton(){}
+
+
+    public static Singleton getInstance(){
+        if(instance != null){
+            return instance;
+        }
+        synchronized (Singleton.class) {
+            if(instance != null) return instance;
+            instance = new Singleton();
+            return instance;
+        }
+    }
+
+    /**
+     * 防止序列化的时候出错；
+     */
+    @Serial
+    private Object readResolve() {
+        return instance;
+    }
+}
