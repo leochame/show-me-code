9c32e23cfdb55ecbf4cbbbe5c38e5bb2c3395cf1
leochame <leocham.cn@gmail.com>
2025-05-11 17:38:32 +0800

    三数之和 5:49

---
+++ written-examination/src/DoublePoint/Solution.java
@@ -0,0 +1,39 @@
+package DoublePoint;
+
+import java.util.ArrayList;
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r){
+                if(nums[i] + nums[l] + nums[r] == 0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[l]);
+                    list.add(nums[r]);
+                    ans.add(list);
+                    l++;
+                    r--;
+                    while( l < r && nums[l] == nums[l-1]){
+                        l++;
+                    }
+                    while( l < r && nums[r] == nums[r+1]){
+                        r--;
+                    }
+                }else if(nums[i] + nums[l] + nums[r] < 0){
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+}
