f3cae22512f72b628f827bed998f7118dfd9de5a
leochame <leocham.cn@gmail.com>
2025-05-12 08:39:45 +0800

    寻找重复数字

--- written-examination/src/other/findDuplicate.java
+++ written-examination/src/other/findDuplicate.java
@@ -7,21 +7,17 @@ public class findDuplicate {
             int fast = 0;
             slow = nums[slow];
             fast = nums[nums[fast]];
-
-            while (slow != fast) {
+            while(slow != fast){
                 slow = nums[slow];
                 fast = nums[nums[fast]];
             }
-
-            int pre1 = 0;
-            int pre2 = slow;
-
-            while (pre1 != pre2) {
+            int pre1 = slow;
+            int pre2 = 0;
+            while(nums[pre1] != nums[pre2]){
                 pre1 = nums[pre1];
                 pre2 = nums[pre2];
             }
-
-            return pre1;
+            return nums[pre1];
         }
     }
 }
