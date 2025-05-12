98c11f2420aa8cc7960238da84a7333d89211154
leochame <leocham.cn@gmail.com>
2025-05-12 08:49:23 +0800

    下一个排列,十分钟内解决

--- written-examination/src/other/nextPermutation.java
+++ written-examination/src/other/nextPermutation.java
@@ -39,3 +39,37 @@ public class nextPermutation {
         }
     }
 }
+class nextPermutation2 {
+    static class Solution {
+        public void nextPermutation(int[] nums) {
+            int n = nums.length;
+            if( n == 1){
+                return;
+            }
+            int i = n - 2;
+            while(i >= 0 && nums[i] >= nums[i+1]){
+                i--;
+            }
+            if(i >= 0){
+                int j = n-1;
+                while(i <= j && nums[i] >= nums[j]){
+                    j--;
+                }
+                swap(nums,j,i);
+            }
+            reverse(nums,i+1,n-1);
+        }
+        public void reverse(int[] nums, int l, int r){
+            while(l < r){
+                swap(nums,l,r);
+                l++;
+                r--;
+            }
+        }
+        public void swap(int[] nums, int i, int j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
